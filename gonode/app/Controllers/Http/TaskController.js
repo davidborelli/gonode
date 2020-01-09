8e6cf165689d2077003040173b886f9bcc1ada3a
David <david_borelli@live.com>
2020-01-09 20:00:58 -0300

    CRUD de tarefas

--- gonode/app/Controllers/Http/TaskController.js
+++ gonode/app/Controllers/Http/TaskController.js
@@ -1,92 +1,91 @@
-'use strict'
+const Task = use('App/Models/Task')
 
-/** @typedef {import('@adonisjs/framework/src/Request')} Request */
-/** @typedef {import('@adonisjs/framework/src/Response')} Response */
-/** @typedef {import('@adonisjs/framework/src/View')} View */
-
-/**
- * Resourceful controller for interacting with tasks
- */
 class TaskController {
-  /**
-   * Show a list of all tasks.
-   * GET tasks
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async index ({ request, response, view }) {
+  async index({ params }) {
+    const tasks = await Task.query()
+      .where('project_id', params.projects_id)
+      .with('user')
+      .fetch()
+
+    return tasks
   }
 
-  /**
-   * Render a form to be used for creating a new task.
-   * GET tasks/create
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async create ({ request, response, view }) {
+  async store({ params, request, response }) {
+    try {
+      const data = request.only([
+        'user_id',
+        'title',
+        'description',
+        'due_date',
+        'file_id',
+      ])
+
+      const task = await Task.create({
+        ...data,
+        project_id: params.projects_id,
+      })
+
+      return task
+    } catch (error) {
+      return response.status(error.status).send({
+        error: {
+          message: `Erro ao criar tarefa. Erro: ${error}`,
+        },
+      })
+    }
   }
 
-  /**
-   * Create/save a new task.
-   * POST tasks
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
-  async store ({ request, response }) {
+  async show({ params, response }) {
+    try {
+      const task = await Task.findOrFail(params.id)
+
+      return task
+    } catch (error) {
+      return response.status(error.status).send({
+        error: {
+          message: `Erro ao localizar tarefa solicitada. Erro: ${error}`,
+        },
+      })
+    }
   }
 
-  /**
-   * Display a single task.
-   * GET tasks/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async show ({ params, request, response, view }) {
+  async update({ params, request, response }) {
+    try {
+      const task = await Task.findOrFail(params.id)
+      const data = request.only([
+        'user_id',
+        'title',
+        'description',
+        'due_date',
+        'file_id',
+      ])
+
+      task.merge(data)
+
+      await task.save()
+
+      return task
+    } catch (error) {
+      return response.status(error.status).send({
+        error: {
+          message: `Erro ao atualizar tarefa solicitada. Erro: ${error}`,
+        },
+      })
+    }
   }
 
-  /**
-   * Render a form to update an existing task.
-   * GET tasks/:id/edit
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async edit ({ params, request, response, view }) {
-  }
+  async destroy({ params, response }) {
+    try {
+      const task = await Task.findOrFail(params.id)
 
-  /**
-   * Update task details.
-   * PUT or PATCH tasks/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
-  async update ({ params, request, response }) {
-  }
-
-  /**
-   * Delete a task with id.
-   * DELETE tasks/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
-  async destroy ({ params, request, response }) {
+      await task.delete()
+    } catch (error) {
+      return response.status(error.status).send({
+        error: {
+          message: `Erro ao deletar tarefa solicitada. Erro: ${error}`,
+        },
+      })
+    }
   }
 }
 
