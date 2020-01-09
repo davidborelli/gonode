addad96c0fe26953c2f6e9e6f992f825340db323
David <david_borelli@live.com>
2020-01-09 19:18:42 -0300

    CRUD de projetos

--- gonode/app/Controllers/Http/ProjectController.js
+++ gonode/app/Controllers/Http/ProjectController.js
@@ -1,17 +1,70 @@
+const Project = use('App/Models/Project')
+
 class ProjectController {
-  async index({ request, response, view }) {}
+  async index() {
+    const projects = await Project.query()
+      .with('user')
+      .fetch()
 
-  async create({ request, response, view }) {}
+    return projects
+  }
 
-  async store({ request, response }) {}
+  async store({ request, auth }) {
+    const data = request.only(['title', 'description'])
 
-  async show({ params, request, response, view }) {}
+    const project = await Project.create({ ...data, user_id: auth.user.id })
 
-  async edit({ params, request, response, view }) {}
+    return project
+  }
 
-  async update({ params, request, response }) {}
+  async show({ params, response }) {
+    try {
+      const project = await Project.findOrFail(params.id)
 
-  async destroy({ params, request, response }) {}
+      await project.load('user')
+      await project.load('tasks')
+
+      return project
+    } catch (error) {
+      return response.status(error.status).send({
+        error: {
+          message: `Erro ao encontar projeto solicitado, verifique o id informado. Erro: ${error}`,
+        },
+      })
+    }
+  }
+
+  async update({ params, request, response }) {
+    try {
+      const project = await Project.findOrFail(params.id)
+      const data = request.only(['title', 'description'])
+
+      project.merge(data)
+
+      await project.save()
+
+      return project
+    } catch (error) {
+      return response.status(error.status).send({
+        error: {
+          message: `Erro ao encontar ou atualizar projeto solicitado, verifique o projeto informado. Erro: ${error}`,
+        },
+      })
+    }
+  }
+
+  async destroy({ params, response }) {
+    try {
+      const project = await Project.findOrFail(params.id)
+      project.delete()
+    } catch (error) {
+      return response.status(error.status).send({
+        error: {
+          message: `Erro ao encontar ou deletar projeto solicitado, verifique o projeto informado. Erro: ${error}`,
+        },
+      })
+    }
+  }
 }
 
 module.exports = ProjectController
