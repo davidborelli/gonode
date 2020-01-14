86e2bf384de1baa5d2cedcbe2ead2feaea68fdaf
David <david_borelli@live.com>
2020-01-13 23:21:35 -0300

    Convites

--- backend/app/Controllers/Http/InviteController.js
+++ backend/app/Controllers/Http/InviteController.js
@@ -1,6 +1,16 @@
+const Invite = use('App/Models/Invite')
+
 class InviteController {
-  async store({ request }) {
-    console.log(request.team)
+  async store({ request, auth }) {
+    const invites = request.input('invites')
+
+    const data = invites.map(email => ({
+      email,
+      user_id: auth.user.id,
+      team_id: request.team.id,
+    }))
+
+    await Invite.createMany(data)
   }
 }
 
