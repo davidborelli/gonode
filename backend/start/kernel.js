b40791638c99db994f2d21937559d05717117836
David <david_borelli@live.com>
2020-01-13 22:47:19 -0300

    Configurando Middleware de Times

--- backend/start/kernel.js
+++ backend/start/kernel.js
@@ -1,6 +1,3 @@
-'use strict'
-
-/** @type {import('@adonisjs/framework/src/Server')} */
 const Server = use('Server')
 
 /*
@@ -14,7 +11,7 @@ const Server = use('Server')
 */
 const globalMiddleware = [
   'Adonis/Middleware/BodyParser',
-  'App/Middleware/ConvertEmptyStringsToNull'
+  'App/Middleware/ConvertEmptyStringsToNull',
 ]
 
 /*
@@ -36,7 +33,8 @@ const globalMiddleware = [
 */
 const namedMiddleware = {
   auth: 'Adonis/Middleware/Auth',
-  guest: 'Adonis/Middleware/AllowGuestOnly'
+  guest: 'Adonis/Middleware/AllowGuestOnly',
+  team: 'App/Middleware/Team',
 }
 
 /*
@@ -51,10 +49,9 @@ const namedMiddleware = {
 */
 const serverMiddleware = [
   // 'Adonis/Middleware/Static',
-  'Adonis/Middleware/Cors'
+  'Adonis/Middleware/Cors',
 ]
 
-Server
-  .registerGlobal(globalMiddleware)
+Server.registerGlobal(globalMiddleware)
   .registerNamed(namedMiddleware)
   .use(serverMiddleware)
