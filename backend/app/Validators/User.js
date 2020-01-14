ebb110a3e24b2e47d340a6e3bf71cd0a2ab945ab
David <david_borelli@live.com>
2020-01-14 14:10:18 -0300

    Inserindo Validações

---
+++ backend/app/Validators/User.js
@@ -0,0 +1,15 @@
+class User {
+  get ValidateAll() {
+    return true
+  }
+
+  get rules() {
+    return {
+      name: 'required',
+      email: 'required|email|unique:users',
+      password: 'required',
+    }
+  }
+}
+
+module.exports = User
