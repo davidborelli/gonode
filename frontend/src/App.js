1c4bc06d0ddb43e67f6559d4830da553b59a90f6
David <david_borelli@live.com>
2020-01-15 18:18:39 -0300

    Configurando alertas com Toastr lib

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import ReduxToastr from 'react-redux-toastr';
 
 import Routes from './routes';
 import store from '~/store';
@@ -11,6 +12,7 @@ export default function App() {
     <Provider store={store}>
       <>
         <Routes />
+        <ReduxToastr timeOut={3000} closeOnToastrClick progressBar />
         <GlobalStyle />
       </>
     </Provider>
