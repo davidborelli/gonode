2ea2ad00dff3cccce3551eea05d500ebda63c5b8
David <david_borelli@live.com>
2020-01-15 18:04:47 -0300

    Autenticação usuário + Alteração CORS liberar frontend

--- frontend/src/Pages/Auth/SignIn/index.js
+++ frontend/src/Pages/Auth/SignIn/index.js
@@ -1,24 +1,73 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import AuthActions from '~/store/ducks/auth';
 
 import * as S from '../styles';
 import Button from '~/styles/components/Button';
 
-export default function SignIn() {
-  return (
-    <S.Container>
-      <S.Form onSubmit={() => {}}>
-        <h1>Bem vindo</h1>
+class SignIn extends Component {
+  static propTypes = {
+    signInRequest: PropTypes.func.isRequired,
+  };
 
-        <span>E-MAIL</span>
-        <input type="email" name="email" />
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
 
-        <span>SENHA</span>
-        <input type="password" name="password" />
+  handleInputChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
 
-        <Button size="big" type="submit">
-          Entrar
-        </Button>
-      </S.Form>
-    </S.Container>
-  );
+  handleSubmit = event => {
+    event.preventDefault();
+
+    const { email, password } = this.state;
+    const { signInRequest } = this.props;
+
+    signInRequest(email, password);
+  };
+
+  render() {
+    const { email, password } = this.state;
+
+    return (
+      <S.Container>
+        <S.Form onSubmit={this.handleSubmit}>
+          <h1>Bem vindo</h1>
+
+          <span>E-MAIL</span>
+          <input
+            type="email"
+            name="email"
+            value={email}
+            onChange={this.handleInputChange}
+          />
+
+          <span>SENHA</span>
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={this.handleInputChange}
+          />
+
+          <Button size="big" type="submit">
+            Entrar
+          </Button>
+        </S.Form>
+      </S.Container>
+    );
+  }
 }
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(AuthActions, dispatch);
+
+export default connect(null, mapDispatchToProps)(SignIn);
---
+++ frontend/src/store/ducks/auth.js
@@ -0,0 +1,25 @@
+import { createReducer, createActions } from 'reduxsauce';
+import Immutable from 'seamless-immutable';
+
+// Actions
+const { Types, Creators } = createActions({
+  signInRequest: ['email', 'password'],
+  signInSuccess: ['token'],
+});
+
+export const AuthTypes = Types;
+export default Creators;
+
+// Reducers
+export const INITIAL_STATE = Immutable({
+  signedIn: false,
+  token: null,
+});
+
+export const success = (state, { token }) =>
+  state.merge({ signedIn: true, token });
+
+// Mapeamento de action chamada para método para executar
+export const reducer = createReducer(INITIAL_STATE, {
+  [Types.SIGN_IN_SUCCESS]: success,
+});
