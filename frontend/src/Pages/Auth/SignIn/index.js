bf45d50e84b562f72c157dec9bf355390e4cde0b
David <david_borelli@live.com>
2020-01-14 21:21:07 -0300

    Layout tela Login + Criar Component Botao global

--- frontend/src/Pages/Auth/SignIn/index.js
+++ frontend/src/Pages/Auth/SignIn/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import * as S from '../styles';
+import Button from '~/styles/components/Button';
 
 export default function SignIn() {
   return (
-    <>
-      <div>SignIn</div>
-    </>
+    <S.Container>
+      <S.Form onSubmit={() => {}}>
+        <h1>Bem vindo</h1>
+
+        <span>E-MAIL</span>
+        <input type="email" name="email" />
+
+        <span>SENHA</span>
+        <input type="password" name="password" />
+
+        <Button size="big" type="submit">
+          Entrar
+        </Button>
+      </S.Form>
+    </S.Container>
   );
 }
