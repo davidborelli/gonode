436c20496328972af6c1fae6a49b7d6415ea0cb4
David <david_borelli@live.com>
2020-01-15 22:36:48 -0300

    Correção key TeamList e PropTypes

--- frontend/src/components/TeamSwitcher/index.js
+++ frontend/src/components/TeamSwitcher/index.js
@@ -9,6 +9,7 @@ import TeamsActions from '~/store/ducks/teams';
 class TeamSwitcher extends Component {
   static propTypes = {
     getTeamsRequest: PropTypes.func.isRequired,
+    teams: PropTypes.oneOfType([PropTypes.func, PropTypes.object]).isRequired,
   };
 
   componentDidMount() {
@@ -24,7 +25,7 @@ class TeamSwitcher extends Component {
       <S.Container>
         <S.TeamList>
           {teams.data.map(team => (
-            <S.Team key={teams.id}>
+            <S.Team key={team.id}>
               <img
                 alt={team.name}
                 src={`https://ui-avatars.com/api/?font-size=0.33&background=7159c1&color=fff&name=${team.name}`}
