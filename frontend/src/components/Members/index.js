19f52fde75f35e78d2c4ae603208c2b2e52445da
David <david_borelli@live.com>
2020-01-16 19:33:02 -0300

    Alterando role do membro e criação componente Input

--- frontend/src/components/Members/index.js
+++ frontend/src/components/Members/index.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import PropTypes from 'prop-types';
+import Select from 'react-select';
+import api from '~/services/api';
 
 import MembersActions from '~/store/ducks/members';
 
@@ -13,6 +15,7 @@ class Members extends Component {
   static propTypes = {
     closeMembersModal: PropTypes.func.isRequired,
     getMembersRequest: PropTypes.func.isRequired,
+    updateMemberRequest: PropTypes.func.isRequired,
     members: PropTypes.shape({
       data: PropTypes.arrayOf(
         PropTypes.shape({
@@ -23,14 +26,31 @@ class Members extends Component {
     }).isRequired,
   };
 
-  componentDidMount() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      roles: [],
+    };
+  }
+
+  async componentDidMount() {
     const { getMembersRequest } = this.props;
 
     getMembersRequest();
+
+    const response = await api.get('roles');
+    this.setState({ roles: response.data });
   }
+
+  handleRolesChange = (id, roles) => {
+    const { updateMemberRequest } = this.props;
+
+    updateMemberRequest(id, roles);
+  };
 
   render() {
     const { closeMembersModal, members } = this.props;
+    const { roles } = this.state;
 
     return (
       <Modal size="big">
@@ -41,6 +61,14 @@ class Members extends Component {
             {members.data.map(member => (
               <li key={member.id}>
                 <strong>{member.user.name}</strong>
+                <Select
+                  isMulti
+                  options={roles}
+                  value={member.roles}
+                  getOptionLabel={role => role.name}
+                  getOptionValue={role => role.id}
+                  onChange={value => this.handleRolesChange(member.id, value)}
+                />
               </li>
             ))}
           </S.MembersList>
--- frontend/src/store/sagas/members.js
+++ frontend/src/store/sagas/members.js
@@ -1,4 +1,5 @@
 import { put, call } from 'redux-saga/effects';
+import { actions as toastrActions } from 'react-redux-toastr';
 import api from '~/services/api';
 
 import MembersActions from '../ducks/members';
@@ -8,4 +9,26 @@ export function* getMembers() {
 
   // Chamando a Action e passando o valor por parâmetro
   yield put(MembersActions.getMembersSuccess(response.data));
+}
+
+export function* updateMember({ id, roles }) {
+  try {
+    yield call(api.put, `members/${id}`, { roles: roles.map(role => role.id) });
+
+    yield put(
+      toastrActions.add({
+        type: 'success',
+        title: 'Sucesso!',
+        message: 'Membro atulizado com sucesso!',
+      })
+    );
+  } catch (error) {
+    yield put(
+      toastrActions.add({
+        type: 'error',
+        title: 'Falha na operação',
+        message: 'Verifique os dados inseridos',
+      })
+    );
+  }
 }
